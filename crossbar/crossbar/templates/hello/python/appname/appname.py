2cf05b72707db0eca470333b8797037db0d4a9a5
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-06-25 19:34:28 +0200

    pimp up template

--- crossbar/crossbar/templates/hello/python/appname/appname.py
+++ crossbar/crossbar/templates/hello/python/appname/appname.py
@@ -1,11 +1,53 @@
+from twisted.internet.defer import inlineCallbacks
+
+from autobahn.twisted.util import sleep
 from autobahn.twisted.wamp import ApplicationSession
+
 
 
 class AppSession(ApplicationSession):
 
+    @inlineCallbacks
     def onJoin(self, details):
 
-        def hello():
-            return "Hello from Python!"
+        ## SUBSCRIBE to a topic and receive events
+        ##
+        def onhello(msg):
+            print("onhello(): {}".format(msg))
 
-        self.register(hello, 'com.{{ appname }}.hello')
+        sub = yield self.subscribe(onhello, 'com.example.onhello')
+        print("subscribed to topic 'onhello'")
+
+
+        ## REGISTER a procedure for remote calling
+        ##
+        def add2(x, y):
+            print("add2() called with {} and {}".format(x, y))
+            return x + y
+
+        reg = yield self.register(add2, 'com.example.add2')
+        print("procedure add2() registered")
+
+
+        ## PUBLISH and CALL every second .. forever
+        ##
+        counter = 0
+        while True:
+
+            ## PUBLISH an event
+            ##
+            yield self.publish('com.example.oncounter', counter)
+            counter += 1
+
+
+            ## CALL a remote procedure
+            ##
+            try:
+                res = yield self.call('com.example.mul2', counter, 3)
+                print("mul2() result: {}".format(res))
+            except Exception as e:
+                print("mul2() error: {}".format(e))
+
+
+            yield sleep(1)
+            print("tick")
