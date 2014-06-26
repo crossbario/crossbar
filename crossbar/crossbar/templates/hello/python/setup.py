ba5181458bf674f5a07cc6070d319a0097314203
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-06-26 14:29:49 +0200

    fix setup script

--- crossbar/crossbar/templates/hello/python/setup.py
+++ crossbar/crossbar/templates/hello/python/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='{{ appname }}',
-    version='0.0.1',
-    description="'{{ appname }}' WAMP Component",
-    platforms=['Any'],
-    packages='{{ appname }}',
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
+    name = '{{ appname }}',
+    version = '0.0.1',
+    description = "'{{ appname }}' WAMP Component",
+    platforms = ['Any'],
+    packages = ['{{ appname }}'],
+    include_package_data = True,
+    zip_safe = False,
+    entry_points = {
         'autobahn.twisted.wamplet': [
             'backend = {{ appname }}.{{ appname }}:AppSession'
         ],
