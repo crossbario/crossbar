afd4830b118c3100abb261a2e2695d20f1387e59
meejah <meejah@meejah.ca>
2015-04-09 19:18:26 -0600

    Add on_component_stop logic
    ...and use it to detect when components shut down "too quickly"
    when we're starting things from a static config.
    
    Also fix some bugs in restart_container_component

--- crossbar/controller/process.py
+++ crossbar/controller/process.py
@@ -528,7 +528,10 @@ class NodeControllerSession(NativeProcessSession):
                 'who': worker.who
             }
 
-            self.publish(started_topic, started_info, options=PublishOptions(exclude=[details.caller]))
+            options = None
+            if details.caller is not None:
+                options = PublishOptions(exclude=[details.caller])
+            self.publish(started_topic, started_info, options=options)
 
             return started_info
 
@@ -569,7 +572,10 @@ class NodeControllerSession(NativeProcessSession):
             details.progress(starting_info)
 
         # .. while all others get an event
-        self.publish(starting_topic, starting_info, options=PublishOptions(exclude=[details.caller]))
+        options=None
+        if details.caller is not None:
+            options = PublishOptions(exclude=[details.caller])
+        self.publish(starting_topic, starting_info, options=options)
 
         # now actually fork the worker ..
         #
