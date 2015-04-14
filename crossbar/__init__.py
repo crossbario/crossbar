8331d8fa6b0dad1027e7a59dd101c0a8c6ff1dbf
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-04-14 15:29:52 +0200

    bump version

--- crossbar/__init__.py
+++ crossbar/__init__.py
@@ -53,4 +53,4 @@ Open-source licensed under the GNU Affero General Public License version 3.
 Created by Tavendo GmbH. Get in contact at http://tavendo.com
 """
 
-__version__ = "0.10.3"
+__version__ = "0.10.4"
