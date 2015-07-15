51c17774ad62eef0121c5a45211eaac60136b40d
HawkOwl <hawkowl@atleastfornow.net>
2015-07-15 16:24:08 +0800

    split the config checking into a separate method

--- crossbar/controller/node.py
+++ crossbar/controller/node.py
@@ -96,6 +96,18 @@ class Node(object):
         # in the node's management router)
         self._controller = None
 
+        # config of this node.
+        self._config = None
+
+    def check_config(self):
+        """
+        Check the configuration of this node.
+        """
+        configfile = os.path.join(self.options.cbdir, self.options.config)
+        self.log.info("Loading node configuration file '{configfile}'",
+                      configfile=configfile)
+        self._config = check_config_file(configfile, silence=True)
+
     @inlineCallbacks
     def start(self):
         """
@@ -104,10 +116,8 @@ class Node(object):
         """
         # for now, a node is always started from a local configuration
         #
-        configfile = os.path.join(self.options.cbdir, self.options.config)
-        self.log.info("Starting from node configuration file '{configfile}'",
-                      configfile=configfile)
-        self._config = check_config_file(configfile, silence=True)
+        if not self._config:
+            self.check_config()
 
         controller_config = self._config.get('controller', {})
 
