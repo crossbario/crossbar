ba48be09af760211bf04e8fa72f80331ae8eae56
HawkOwl <hawkowl@atleastfornow.net>
2015-08-28 13:50:36 +0800

    fix more tests on Windows

--- crossbar/router/test/test_observation.py
+++ crossbar/router/test/test_observation.py
@@ -36,6 +36,7 @@ from io import BytesIO as StringIO
 
 from autobahn.wamp.message import Subscribe
 
+from crossbar._compat import long
 from crossbar.router.observation import ExactUriObservation, \
     PrefixUriObservation, WildcardUriObservation, UriObservationMap
 
@@ -51,7 +52,7 @@ class TestObservation(unittest.TestCase):
         Create an exact-matching observation.
         """
         obs1 = ExactUriObservation(u"com.example.uri1")
-        self.assertEqual(type(obs1.id), int)
+        self.assertEqual(type(obs1.id), (int, long))
         self.assertEqual(obs1.uri, u"com.example.uri1")
         self.assertEqual(obs1.match, u"exact")
         self.assertEqual(obs1.observers, set())
@@ -61,7 +62,7 @@ class TestObservation(unittest.TestCase):
         Create a prefix-matching observation.
         """
         obs1 = PrefixUriObservation(u"com.example.uri1")
-        self.assertEqual(type(obs1.id), int)
+        self.assertEqual(type(obs1.id), (int, long))
         self.assertEqual(obs1.uri, u"com.example.uri1")
         self.assertEqual(obs1.match, u"prefix")
         self.assertEqual(obs1.observers, set())
@@ -71,7 +72,7 @@ class TestObservation(unittest.TestCase):
         Create a wildcard-matching observation.
         """
         obs1 = WildcardUriObservation(u"com.example..create")
-        self.assertEqual(type(obs1.id), int)
+        self.assertEqual(type(obs1.id), (int, long))
         self.assertEqual(obs1.uri, u"com.example..create")
         self.assertEqual(obs1.match, u"wildcard")
         self.assertEqual(obs1.observers, set())
