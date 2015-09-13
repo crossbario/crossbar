d51a6bed0731d36a9f807371156a1b000bc072b7
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-09-13 21:37:52 +0200

    bump deps; bump version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ else:
 #
 
 extras_require_system = [
-    'psutil>=3.1.1',        # BSD license
+    'psutil>=3.2.1',        # BSD license
 ]
 if sys.platform.startswith('linux'):
     extras_require_system.append('setproctitle>=1.1.9')     # BSD license
@@ -88,10 +88,17 @@ extras_require_tls = [
     'service_identity>=14.0.0',     # MIT license
 ]
 
-extras_require_accelerate = [
-    "wsaccel>=0.6.2",       # Apache license
-    "ujson>=1.33"           # BSD License
-] if CPY else []  # only for CPy (skip for PyPy)!
+# only for CPy (skip for PyPy)!
+if CPY:
+    extras_require_accelerate = [
+        "wsaccel>=0.6.2"            # Apache 2.0
+    ]
+
+    # ujson is broken on Windows (https://github.com/esnme/ultrajson/issues/184)
+    if sys.platform != 'win32':
+        extras_require_accelerate.append("ujson>=1.33")     # BSD license
+else:
+    extras_require_accelerate = []
 
 # Extra requirements which enhance the development experience
 extras_require_dev = [
@@ -122,15 +129,15 @@ setup(
     url='http://crossbar.io/',
     platforms=('Any'),
     install_requires=[
-        'click>=4.1',                 # BSD license
-        'setuptools>=18.1',           # Python Software Foundation license
-        'zope.interface>=3.6.0',      # Zope Public license
-        'twisted>=15.3.0',            # MIT license
-        'autobahn[twisted]>=0.10.7',  # MIT license
-        'netaddr>=0.7.15',            # BSD license
+        'click>=5.1',                 # BSD license
+        'setuptools>=18.3.1',         # Python Software Foundation license
+        'zope.interface>=4.1.2',      # Zope Public license
+        'twisted>=15.4.0',            # MIT license
+        'autobahn[twisted]>=0.10.8',  # MIT license
+        'netaddr>=0.7.18',            # BSD license
         'pytrie>=0.2',                # BSD license
         'jinja2>=2.8',                # BSD license
-        'mistune>=0.7',               # BSD license
+        'mistune>=0.7.1',             # BSD license
         'pygments>=2.0.2',            # BSD license
         'pyyaml>=3.11',               # MIT license
         'shutilwhich>=1.1.0',         # PSF license
