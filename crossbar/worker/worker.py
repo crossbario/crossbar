f04da305cdf3d1324b49c4343ed675d9c6f9f498
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-10-03 14:29:11 +0200

    document and polish the management API

--- crossbar/worker/worker.py
+++ crossbar/worker/worker.py
@@ -80,6 +80,8 @@ class NativeWorkerSession(NativeProcessSession):
 
         self._module_tracker = TrackingModuleReloader(debug=True)
 
+        self._profiles = {}
+
         self.join(self.config.realm)
 
     @inlineCallbacks
@@ -91,6 +93,7 @@ class NativeWorkerSession(NativeProcessSession):
 
         procs = [
             # CPU affinity for this worker process
+            'get_cpu_count',
             'get_cpu_affinity',
             'set_cpu_affinity',
 
@@ -101,7 +104,7 @@ class NativeWorkerSession(NativeProcessSession):
             # profiling control
             'get_profilers',
             'start_profiler',
-            'query_profile',
+            'get_profile',
         ]
 
         dl = []
@@ -132,14 +135,35 @@ class NativeWorkerSession(NativeProcessSession):
 
     def get_profilers(self, details=None):
         """
+        Registered under: ``crossbar.node.<node_id>.worker.<worker_id>.get_profilers``
+
         Returns available profilers.
+
+        :param details: WAMP call details (auto-filled by WAMP).
+        :type details: obj
+        :returns: A list of profilers.
+        :rtype: list of unicode
         """
         return [p.marshal() for p in PROFILERS.items()]
 
     def start_profiler(self, profiler, runtime=10, async=True, details=None):
         """
+        Registered under: ``crossbar.node.<node_id>.worker.<worker_id>.start_profiler``
+
         Start a profiler producing a profile which is stored and can be
         queried later.
+
+        :param profiler: The profiler to start, e.g. ``vmprof``.
+        :type profiler: unicode
+        :param runtime: Profiling duration in seconds.
+        :type runtime: float
+        :param async: Flag to turn on/off asynchronous mode.
+        :type async: bool
+        :param details: WAMP call details (auto-filled by WAMP).
+        :type details: obj
+        :returns: If running in synchronous mode, the profiling result. Else
+            a profile ID is returned which later can be used to retrieve the profile.
+        :rtype: dict or int
         """
         if profiler not in PROFILERS:
             raise Exception("no such profiler")
@@ -152,16 +176,62 @@ class NativeWorkerSession(NativeProcessSession):
         # that will fire with the actual profile recorded
         profile_id, profile_finished = profiler.start(runtime=runtime)
 
-        def on_profile_finished(res):
-            # FIXME: store the profile in the node database (LMDB)
-            # print("profile stored in {}".format(profile_filename))
-            return res
+        on_profile_started = u'crossbar.node.{}.worker.{}.on_profile_started'.format(self.config.extra.node, self.config.extra.worker)
+        on_profile_finished = u'crossbar.node.{}.worker.{}.on_profile_finished'.format(self.config.extra.node, self.config.extra.worker)
 
-        def on_profile_failed(err):
-            print("profile failed: {}".format(err))
-            return err
+        if async:
+            publish_options = None
+        else:
+            publish_options = PublishOptions(exclude=[details.caller])
 
-        # profile_finished.addCallbacks(on_profile_finished, on_profile_failed)
+        self.publish(
+            on_profile_started,
+            {
+                u'id': profile_id,
+                u'who': details.caller,
+                u'profiler': profiler,
+                u'runtime': runtime,
+                u'async': async,
+            },
+            options=publish_options
+        )
+
+        def on_profile_success(profile_result):
+            self._profiles[profile_id] = {
+                u'id': profile_id,
+                u'profiler': profiler,
+                u'runtime': runtime,
+                u'profile': profile_result
+            }
+
+            self.publish(
+                on_profile_finished,
+                {
+                    u'id': profile_id,
+                    u'error': None,
+                    u'profile': profile_result
+                },
+                options=publish_options
+            )
+
+            return profile_result
+
+        def on_profile_failed(error):
+            self.log.warn('profiling failed: {error}', error=error)
+
+            self.publish(
+                on_profile_finished,
+                {
+                    u'id': profile_id,
+                    u'error': u'{0}'.format(error),
+                    u'profile': None
+                },
+                options=publish_options
+            )
+
+            return error
+
+        profile_finished.addCallbacks(on_profile_success, on_profile_failed)
 
         if async:
             # if running in async mode, immediately return the ID under
@@ -172,22 +242,62 @@ class NativeWorkerSession(NativeProcessSession):
             # actually finished - and return the complete profile
             return profile_finished
 
-    def query_profile(self, profile_id, query, wait_on_profile=True, details=None):
+    def get_profile(self, profile_id, details=None):
         """
-        Query a profile previously produced by a profiler run.
+        Get a profile previously produced by a profiler run.
+
+        This procedure is registered under WAMP URI
+        ``crossbar.node.<node_id>.worker.<worker_id>.get_profile``.
+
+        When no profile with given ID exists, a WAMP error
+        ``crossbar.error.no_such_object`` is raised.
         """
-        pass
+        if profile_id in self._profiles:
+            return self._profiles[profile_id]
+        else:
+            raise ApplicationError(u'crossbar.error.no_such_object', 'no profile with ID {} saved'.format(profile_id))
+
+    def get_cpu_count(self, logical=True):
+        """
+        Returns the CPU core count on the machine this process is running on.
+
+        This procedure is registered under WAMP URI
+        ``crossbar.node.<node_id>.worker.<worker_id>.get_cpu_count``.
+
+        When the required support packages are not installed, raises a WAMP error
+        ``crossbar.error.feature_unavailable``.
+
+        :param logical: If enabled (default), include logical CPU cores ("Hyperthreading"),
+            else only count physical CPU cores.
+        :type logical: bool
+        :returns: The number of CPU cores.
+        :rtype: int
+        """
+        if not _HAS_PSUTIL:
+            emsg = "unable to get CPU count: required package 'psutil' is not installed"
+            self.log.warn(emsg)
+            raise ApplicationError(u"crossbar.error.feature_unavailable", emsg)
+
+        return psutil.cpu_count(logical=logical)
 
     def get_cpu_affinity(self, details=None):
         """
         Get CPU affinity of this process.
 
-        :returns list -- List of CPU IDs the process affinity is set to.
-        """
-        self.log.debug("{klass}.get_cpu_affinity", klass=self.__class__.__name__)
+        This procedure is registered under WAMP URI
+        ``crossbar.node.<node_id>.worker.<worker_id>.get_cpu_affinity``.
 
+        When the required support packages are not installed, raises a WAMP error
+        ``crossbar.error.feature_unavailable``.
+
+        When the CPU affinity cannot be determined, a WAMP error
+        ``crossbar.error.runtime_error`` is raised.
+
+        :returns: List of CPU IDs the process affinity is set to.
+        :rtype: list of int
+        """
         if not _HAS_PSUTIL:
-            emsg = "Unable to get CPU affinity: required package 'psutil' is not installed"
+            emsg = "unable to get CPU affinity: required package 'psutil' is not installed"
             self.log.warn(emsg)
             raise ApplicationError(u"crossbar.error.feature_unavailable", emsg)
 
@@ -199,18 +309,26 @@ class NativeWorkerSession(NativeProcessSession):
             self.log.failure(emsg)
             raise ApplicationError(u"crossbar.error.runtime_error", emsg)
         else:
-            res = {'affinity': current_affinity}
-            return res
+            return current_affinity
 
     def set_cpu_affinity(self, cpus, details=None):
         """
         Set CPU affinity of this process.
 
-        :param cpus: List of CPU IDs to set process affinity to.
-        :type cpus: list
-        """
-        self.log.debug("{klass}.set_cpu_affinity", klass=self.__class__.__name__)
+        This procedure is registered under WAMP URI
+        ``crossbar.node.<node_id>.worker.<worker_id>.set_cpu_affinity``.
 
+        When the CPU affinity has been successfully set, a WAMP event is published
+        to ``crossbar.node.{}.worker.{}.on_cpu_affinity_set``
+
+        When the required support packages are not installed, raises a WAMP error
+        ``crossbar.error.feature_unavailable``.
+
+        :param cpus: List of CPU IDs to set process affinity to. Each CPU ID must be
+            from the list `[0 .. N_CPUs]`, where N_CPUs can be retrieved via
+            ``crossbar.node.<node_id>.worker.<worker_id>.get_cpu_count``.
+        :type cpus: list of int
+        """
         if not _HAS_PSUTIL:
             emsg = "Unable to set CPU affinity: required package 'psutil' is not installed"
             self.log.warn(emsg)
@@ -228,7 +346,7 @@ class NativeWorkerSession(NativeProcessSession):
 
             # publish info to all but the caller ..
             #
-            cpu_affinity_set_topic = 'crossbar.node.{}.worker.{}.on_cpu_affinity_set'.format(self.config.extra.node, self.config.extra.worker)
+            cpu_affinity_set_topic = u'crossbar.node.{}.worker.{}.on_cpu_affinity_set'.format(self.config.extra.node, self.config.extra.worker)
             cpu_affinity_set_info = {
                 'affinity': new_affinity,
                 'who': details.caller
@@ -237,13 +355,17 @@ class NativeWorkerSession(NativeProcessSession):
 
             # .. and return info directly to caller
             #
-            return cpu_affinity_set_info
+            return new_affinity
 
     def get_pythonpath(self, details=None):
         """
         Returns the current Python module search paths.
 
-        :returns list -- List of module search paths.
+        This procedure is registered under WAMP URI
+        ``crossbar.node.<node_id>.worker.<worker_id>.get_pythonpath``.
+
+        :returns: The current module search paths.
+        :rtype: list of unicode
         """
         self.log.debug("{klass}.get_pythonpath", klass=self.__class__.__name__)
         return sys.path
@@ -252,9 +374,12 @@ class NativeWorkerSession(NativeProcessSession):
         """
         Add paths to Python module search paths.
 
+        This procedure is registered under WAMP URI
+        ``crossbar.node.<node_id>.worker.<worker_id>.add_pythonpath``.
+
         :param paths: List of paths. Relative paths will be resolved relative
                       to the node directory.
-        :type paths: list
+        :type paths: list of unicode
         :param prepend: If `True`, prepend the given paths to the current paths.
                         Otherwise append.
         :type prepend: bool
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+Crossbar.io Management API
+==========================
+
+This document describes the **Crossbar.io Management API**.
+
+The copyright for **both** this document **and** the **Crossbar.io Management API** defined herein is owned by `Tavendo GmbH <http://tavendo.com/>`__. The copyright on the API covers the `structure, sequence and organization (SSO) <https://en.wikipedia.org/wiki/Structure,_sequence_and_organization>`__ of the API.
+
+
+Contents:
+
+.. toctree::
+   :maxdepth: 1
+
+   native-workers
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
