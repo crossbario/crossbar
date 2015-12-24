bf2cde4cc09636eecfd1c10ee06ed50a69a5bfee
Tobias Oberstein <tobias.oberstein@tavendo.de>
2015-12-24 10:17:39 +0100

    cleanup

--- crossbar/twisted/endpoint.py
+++ crossbar/twisted/endpoint.py
@@ -67,6 +67,187 @@ __all__ = ('create_listening_endpoint_from_config',
            'create_connecting_port_from_config')
 
 
+def _create_tls_server_context(log, config, cbdir):
+    """
+    Create a CertificateOptions object for use with TLS listening endpoints.
+    """
+    # server private key
+    key_filepath = abspath(join(cbdir, config['key']))
+    log.info("Loading server TLS key from {key_filepath}", key_filepath=key_filepath)
+    with open(key_filepath) as key_file:
+        # server certificate (but only the server cert, no chain certs)
+        cert_filepath = abspath(join(cbdir, config['certificate']))
+        log.info("Loading server TLS certificate from {cert_filepath}", cert_filepath=cert_filepath)
+        with open(cert_filepath) as cert_file:
+            key = KeyPair.load(key_file.read(), crypto.FILETYPE_PEM).original
+            cert = Certificate.loadPEM(cert_file.read()).original
+
+    # list of certificates that complete your verification chain
+    extra_certs = None
+    if 'chain_certificates' in config:
+        extra_certs = []
+        for fname in config['chain_certificates']:
+            extra_cert_filepath = abspath(join(cbdir, fname))
+            with open(extra_cert_filepath, 'r') as f:
+                extra_certs.append(Certificate.loadPEM(f.read()).original)
+            log.info("Loading server TLS chain certificate from {extra_cert_filepath}", extra_cert_filepath=extra_cert_filepath)
+
+    # list of certificate authority certificate objects to use to verify the peer's certificate
+    ca_certs = None
+    if 'ca_certificates' in config:
+        ca_certs = []
+        for fname in config['ca_certificates']:
+            ca_cert_filepath = abspath(join(cbdir, fname))
+            with open(ca_cert_filepath, 'r') as f:
+                ca_certs.append(Certificate.loadPEM(f.read()).original)
+            log.info("Loading server TLS CA certificate from {ca_cert_filepath}", ca_cert_filepath=ca_cert_filepath)
+
+    # ciphers we accept
+    #
+    # We prefer to make every single cipher (6 in total) _explicit_ (to reduce chances either we or the pattern-matching
+    # language inside OpenSSL messes up) and drop support for Windows XP (we do WebSocket anyway).
+    #
+    # We don't use AES256 and SHA384, to reduce number of ciphers and since the additional
+    # security gain seems not worth the additional performance drain.
+    #
+    # We also don't use ECDSA, since EC certificates a rare in the wild.
+    #
+    # The effective list of ciphers determined from an OpenSSL cipher string:
+    #
+    #   openssl ciphers -v 'ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:'
+    #
+    # References:
+    #
+    #  * https://www.ssllabs.com/ssltest/analyze.html?d=myserver.com
+    #  * http://hynek.me/articles/hardening-your-web-servers-ssl-ciphers/
+    #  * http://www.openssl.org/docs/apps/ciphers.html#CIPHER_LIST_FORMAT
+    #  * https://wiki.mozilla.org/Talk:Security/Server_Side_TLS
+    #
+    if 'ciphers' in config:
+        log.info("Using explicit TLS ciphers from config")
+        crossbar_ciphers = AcceptableCiphers.fromOpenSSLCipherString(config['ciphers'])
+    else:
+        log.info("Using secure default TLS ciphers")
+        crossbar_ciphers = AcceptableCiphers.fromOpenSSLCipherString(
+            # AEAD modes (GCM)
+            # 'ECDHE-ECDSA-AES128-GCM-SHA256:'
+            'ECDHE-RSA-AES128-GCM-SHA256:'
+            # 'ECDHE-ECDSA-AES256-GCM-SHA384:'
+            # 'ECDHE-RSA-AES256-GCM-SHA384:'
+            'DHE-RSA-AES128-GCM-SHA256:'
+            # 'DHE-RSA-AES256-GCM-SHA384:'
+
+            # CBC modes
+            'ECDHE-RSA-AES128-SHA256:'
+            'DHE-RSA-AES128-SHA256:'
+            'ECDHE-RSA-AES128-SHA:'
+            'DHE-RSA-AES128-SHA:'
+        )
+
+    # DH modes require a parameter file
+    if 'dhparam' in config:
+        dhpath = FilePath(abspath(join(cbdir, config['dhparam'])))
+        dh_params = DiffieHellmanParameters.fromFile(dhpath)
+    else:
+        dh_params = None
+        log.warn("No OpenSSL DH parameter file set - DH cipher modes will be deactive!")
+
+    # create a TLS context factory
+    # see: https://twistedmatrix.com/documents/current/api/twisted.internet.ssl.CertificateOptions.html
+    ctx = CertificateOptions(
+        privateKey=key,
+        certificate=cert,
+        extraCertChain=extra_certs,
+        verify=(ca_certs is not None),
+        caCerts=ca_certs,
+        dhParameters=dh_params,
+        acceptableCiphers=crossbar_ciphers,
+    )
+
+    # Without a curve being set, ECDH won't be available even if listed
+    # in acceptable ciphers!
+    #
+    # The curves available in OpenSSL can be listed:
+    #
+    #   openssl ecparam -list_curves
+    #
+    # prime256v1: X9.62/SECG curve over a 256 bit prime field
+    #
+    # This is elliptic curve "NIST P-256" from here
+    # http://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-4.pdf
+    #
+    # This seems to be the most widely used curve
+    #
+    # http://crypto.stackexchange.com/questions/11310/with-openssl-and-ecdhe-how-to-show-the-actual-curve-being-used
+    #
+    # and researchers think it is "ok" (other than wrt timing attacks etc)
+    #
+    # https://twitter.com/hyperelliptic/status/394258454342148096
+    #
+    if ctx._ecCurve is None:
+        log.warn("No OpenSSL elliptic curve set - EC cipher modes will be deactive!")
+    else:
+        if ctx._ecCurve.snName != "prime256v1":
+            log.info("OpenSSL is using elliptic curve {curve}", curve=ctx._ecCurve.snName)
+        else:
+            log.info("OpenSSL is using most common elliptic curve (prime256v1 / NIST P-256)")
+
+    return ctx
+
+
+def _create_tls_client_context(log, config, cbdir):
+    """
+    Create a CertificateOptions object for use with TLS listening endpoints.
+    """
+    # server hostname: The expected name of the remote host.
+    hostname = config['hostname']
+
+    # explicit trust (certificate) root
+    ca_certs = None
+    if 'ca_certificates' in config:
+        log.info("TLS client using explicit trust ({cnt_certs} certificates)", cnt_certs=len(config['ca_certificates']))
+        ca_certs = []
+        for cert_fname in [os.path.abspath(os.path.join(cbdir, x)) for x in (config['ca_certificates'])]:
+            cert = crypto.load_certificate(
+                crypto.FILETYPE_PEM,
+                six.u(open(cert_fname, 'r').read())
+            )
+            log.info("TLS client trust root CA certificate loaded from '{fname}'", fname=cert_fname)
+            ca_certs.append(cert)
+        ca_certs = OpenSSLCertificateAuthorities(ca_certs)
+    else:
+        log.info("TLS client using platform trust")
+
+    # client key/cert to use
+    client_cert = None
+    if 'key' in config:
+        if 'certificate' not in config:
+            raise Exception('TLS client key present, but certificate missing')
+
+        key_fname = os.path.abspath(os.path.join(cbdir, config['key']))
+        with open(key_fname, 'r') as f:
+            private_key = KeyPair.load(f.read(), format=crypto.FILETYPE_PEM)
+            log.info("Loaded client TLS key from '{key_fname}'", key_fname=key_fname)
+
+        cert_fname = os.path.abspath(os.path.join(cbdir, config['certificate']))
+        with open(cert_fname, 'r') as f:
+            cert = Certificate.loadPEM(f.read(),)
+            log.info("Loaded client TLS certificate from '{cert_fname}' (cn='{cert_cn}', sha256={cert_sha256}..)",
+                     cert_fname=cert_fname,
+                     cert_cn=cert.getSubject().CN,
+                     cert_sha256=cert.digest('sha256')[:12])
+
+        client_cert = PrivateCertificate.fromCertificateAndKeyPair(cert, private_key)
+    else:
+        if 'certificate' in config:
+            log.warn('TLS client certificate present, but key is missing')
+
+    # create TLS client context
+    ctx = optionsForClientTLS(hostname, trustRoot=ca_certs, clientCertificate=client_cert)
+
+    return ctx
+
+
 def create_listening_endpoint_from_config(config, cbdir, reactor):
     """
     Create a Twisted stream server endpoint from a Crossbar.io transport configuration.
@@ -114,135 +295,16 @@ def create_listening_endpoint_from_config(config, cbdir, reactor):
         backlog = int(config.get('backlog', 50))
 
         if 'tls' in config:
+            # create a TLS server endpoint
+            #
             if _HAS_TLS:
+                # TLS server context
+                context = _create_tls_server_context(log, config['tls'], cbdir)
 
-                # server private key
-                key_filepath = abspath(join(cbdir, config['tls']['key']))
-                log.info("Loading server TLS key from {key_filepath}", key_filepath=key_filepath)
-                with open(key_filepath) as key_file:
-                    # server certificate (but only the server cert, no chain certs)
-                    cert_filepath = abspath(join(cbdir, config['tls']['certificate']))
-                    log.info("Loading server TLS certificate from {cert_filepath}", cert_filepath=cert_filepath)
-                    with open(cert_filepath) as cert_file:
-                        key = KeyPair.load(key_file.read(), crypto.FILETYPE_PEM).original
-                        cert = Certificate.loadPEM(cert_file.read()).original
-
-                # list of certificates that complete your verification chain
-                extra_certs = None
-                if 'chain_certificates' in config['tls']:
-                    extra_certs = []
-                    for fname in config['tls']['chain_certificates']:
-                        extra_cert_filepath = abspath(join(cbdir, fname))
-                        with open(extra_cert_filepath, 'r') as f:
-                            extra_certs.append(Certificate.loadPEM(f.read()).original)
-                        log.info("Loading server TLS chain certificate from {extra_cert_filepath}", extra_cert_filepath=extra_cert_filepath)
-
-                # list of certificate authority certificate objects to use to verify the peer's certificate
-                ca_certs = None
-                if 'ca_certificates' in config['tls']:
-                    ca_certs = []
-                    for fname in config['tls']['ca_certificates']:
-                        ca_cert_filepath = abspath(join(cbdir, fname))
-                        with open(ca_cert_filepath, 'r') as f:
-                            ca_certs.append(Certificate.loadPEM(f.read()).original)
-                        log.info("Loading server TLS CA certificate from {ca_cert_filepath}", ca_cert_filepath=ca_cert_filepath)
-
-                # ciphers we accept
-                #
-                # We prefer to make every single cipher (6 in total) _explicit_ (to reduce chances either we or the pattern-matching
-                # language inside OpenSSL messes up) and drop support for Windows XP (we do WebSocket anyway).
-                #
-                # We don't use AES256 and SHA384, to reduce number of ciphers and since the additional
-                # security gain seems not worth the additional performance drain.
-                #
-                # We also don't use ECDSA, since EC certificates a rare in the wild.
-                #
-                # The effective list of ciphers determined from an OpenSSL cipher string:
-                #
-                #   openssl ciphers -v 'ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA256:'
-                #
-                # References:
-                #
-                #  * https://www.ssllabs.com/ssltest/analyze.html?d=myserver.com
-                #  * http://hynek.me/articles/hardening-your-web-servers-ssl-ciphers/
-                #  * http://www.openssl.org/docs/apps/ciphers.html#CIPHER_LIST_FORMAT
-                #  * https://wiki.mozilla.org/Talk:Security/Server_Side_TLS
-                #
-                if 'ciphers' in config['tls']:
-                    log.info("Using explicit TLS ciphers from config")
-                    crossbar_ciphers = AcceptableCiphers.fromOpenSSLCipherString(config['tls']['ciphers'])
-                else:
-                    log.info("Using secure default TLS ciphers")
-                    crossbar_ciphers = AcceptableCiphers.fromOpenSSLCipherString(
-                        # AEAD modes (GCM)
-                        # 'ECDHE-ECDSA-AES128-GCM-SHA256:'
-                        'ECDHE-RSA-AES128-GCM-SHA256:'
-                        # 'ECDHE-ECDSA-AES256-GCM-SHA384:'
-                        # 'ECDHE-RSA-AES256-GCM-SHA384:'
-                        'DHE-RSA-AES128-GCM-SHA256:'
-                        # 'DHE-RSA-AES256-GCM-SHA384:'
-
-                        # CBC modes
-                        'ECDHE-RSA-AES128-SHA256:'
-                        'DHE-RSA-AES128-SHA256:'
-                        'ECDHE-RSA-AES128-SHA:'
-                        'DHE-RSA-AES128-SHA:'
-                    )
-
-                # DH modes require a parameter file
-                if 'dhparam' in config['tls']:
-                    dhpath = FilePath(abspath(join(cbdir, config['tls']['dhparam'])))
-                    dh_params = DiffieHellmanParameters.fromFile(dhpath)
-                else:
-                    dh_params = None
-                    log.warn("No OpenSSL DH parameter file set - DH cipher modes will be deactive!")
-
-                # create a TLS context factory
-                # see: https://twistedmatrix.com/documents/current/api/twisted.internet.ssl.CertificateOptions.html
-                ctx = CertificateOptions(
-                    privateKey=key,
-                    certificate=cert,
-                    extraCertChain=extra_certs,
-                    verify=(ca_certs is not None),
-                    caCerts=ca_certs,
-                    dhParameters=dh_params,
-                    acceptableCiphers=crossbar_ciphers,
-                )
-
-                # Without a curve being set, ECDH won't be available even if listed
-                # in acceptable ciphers!
-                #
-                # The curves available in OpenSSL can be listed:
-                #
-                #   openssl ecparam -list_curves
-                #
-                # prime256v1: X9.62/SECG curve over a 256 bit prime field
-                #
-                # This is elliptic curve "NIST P-256" from here
-                # http://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.186-4.pdf
-                #
-                # This seems to be the most widely used curve
-                #
-                # http://crypto.stackexchange.com/questions/11310/with-openssl-and-ecdhe-how-to-show-the-actual-curve-being-used
-                #
-                # and researchers think it is "ok" (other than wrt timing attacks etc)
-                #
-                # https://twitter.com/hyperelliptic/status/394258454342148096
-                #
-                if ctx._ecCurve is None:
-                    log.warn("No OpenSSL elliptic curve set - EC cipher modes will be deactive!")
-                else:
-                    if ctx._ecCurve.snName != "prime256v1":
-                        log.info("OpenSSL is using elliptic curve {curve}", curve=ctx._ecCurve.snName)
-                    else:
-                        log.info("OpenSSL is using most common elliptic curve (prime256v1 / NIST P-256)")
-
-                # create a TLS server endpoint
-                #
                 if version == 4:
                     endpoint = SSL4ServerEndpoint(reactor,
                                                   port,
-                                                  ctx,
+                                                  context,
                                                   backlog=backlog,
                                                   interface=interface)
                 elif version == 6:
@@ -386,60 +448,18 @@ def create_connecting_endpoint_from_config(config, cbdir, reactor):
         timeout = int(config.get('timeout', 10))
 
         if 'tls' in config:
+            # create a TLS client endpoint
+            #
             if _HAS_TLS:
-                # server hostname: The expected name of the remote host.
-                hostname = config['tls']['hostname']
+                # TLS client context
+                context = _create_tls_client_context(log, config['tls'], cbdir)
 
-                # explicit trust (certificate) root
-                ca_certs = None
-                if 'ca_certificates' in config['tls']:
-                    log.info("TLS client using explicit trust ({cnt_certs} certificates)", cnt_certs=len(config['tls']['ca_certificates']))
-                    ca_certs = []
-                    for cert_fname in [os.path.abspath(os.path.join(cbdir, x)) for x in (config['tls']['ca_certificates'])]:
-                        cert = crypto.load_certificate(
-                            crypto.FILETYPE_PEM,
-                            six.u(open(cert_fname, 'r').read())
-                        )
-                        log.info("TLS client trust root CA certificate loaded from '{fname}'", fname=cert_fname)
-                        ca_certs.append(cert)
-                    ca_certs = OpenSSLCertificateAuthorities(ca_certs)
-                else:
-                    log.info("TLS client using platform trust")
-
-                # client key/cert to use
-                client_cert = None
-                if 'key' in config['tls']:
-                    if 'certificate' not in config['tls']:
-                        raise Exception('TLS client key present, but certificate missing')
-
-                    key_fname = os.path.abspath(os.path.join(cbdir, config['tls']['key']))
-                    with open(key_fname, 'r') as f:
-                        private_key = KeyPair.load(f.read(), format=crypto.FILETYPE_PEM)
-                        log.info("Loaded client TLS key from '{key_fname}'", key_fname=key_fname)
-
-                    cert_fname = os.path.abspath(os.path.join(cbdir, config['tls']['certificate']))
-                    with open(cert_fname, 'r') as f:
-                        cert = Certificate.loadPEM(f.read(),)
-                        log.info("Loaded client TLS certificate from '{cert_fname}' (cn='{cert_cn}', sha256={cert_sha256}..)",
-                                 cert_fname=cert_fname,
-                                 cert_cn=cert.getSubject().CN,
-                                 cert_sha256=cert.digest('sha256')[:12])
-
-                    client_cert = PrivateCertificate.fromCertificateAndKeyPair(cert, private_key)
-                else:
-                    if 'certificate' in config['tls']:
-                        log.warn('TLS client certificate present, but key is missing')
-
-                # create TLS client options
-                options = optionsForClientTLS(hostname, trustRoot=ca_certs, clientCertificate=client_cert)
-
-                # create a TLS client endpoint
                 if version == 4:
                     endpoint = SSL4ClientEndpoint(
                         reactor,
                         host,
                         port,
-                        options,
+                        context,
                         timeout=timeout,
                     )
                 elif version == 6:
