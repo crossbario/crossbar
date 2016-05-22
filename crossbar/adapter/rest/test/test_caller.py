58da64eeb86e5de43479d77063afda2166611c42
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-05-22 23:26:46 +0800

    some test changes

--- crossbar/adapter/rest/test/test_caller.py
+++ crossbar/adapter/rest/test/test_caller.py
@@ -34,13 +34,27 @@ import json
 
 from twisted.internet.defer import inlineCallbacks, maybeDeferred
 
+
+from autobahn.wamp import types
+from autobahn.wamp import message
+from autobahn.wamp import role
 from autobahn.wamp.exception import ApplicationError
+from autobahn.twisted.wamp import ApplicationSession
+
+from crossbar.worker.router import RouterRealm
+from crossbar.router.router import RouterFactory
+from crossbar.router.session import RouterSessionFactory, RouterSession
+from crossbar.router.broker import Broker
+from crossbar.router.role import RouterRoleStaticAuth
 
 from crossbar.test import TestCase
 from crossbar._compat import native_string
 from crossbar._logging import LogCapturer
 from crossbar.adapter.rest import CallerResource
 from crossbar.adapter.rest.test import renderResource
+
+
+
 
 
 class MockSession(object):
@@ -102,19 +116,59 @@ class CallerTestCase(TestCase):
     Unit tests for L{CallerResource}. These tests make no WAMP calls, but test
     the interaction of the calling HTTP request and the resource.
     """
+    def setUp(self):
+
+        # create a router factory
+        self.router_factory = RouterFactory(u'mynode')
+
+        # start a realm
+        self.realm = RouterRealm(None, {u'name': u'realm1'})
+        self.router_factory.start_realm(self.realm)
+
+        # allow everything
+        self.router = self.router_factory.get(u'realm1')
+        self.router.add_role(
+            RouterRoleStaticAuth(
+                self.router,
+                u'test_role',
+                default_permissions={
+                    u'uri': u'com.test.',
+                    u'match': u'prefix',
+                    u'allow': {
+                        u'call': True,
+                        u'register': True,
+                        u'publish': True,
+                        u'subscribe': True,
+                    }
+                }
+            )
+        )
+
+        # create a router session factory
+        self.session_factory = RouterSessionFactory(self.router_factory)
+
+
     @inlineCallbacks
     def test_add2(self):
         """
         Test a very basic call where you add two numbers together. This has two
         args, no kwargs, and no authorisation.
         """
-        session = MockSession(self)
-        session._addProcedureCall("com.test.add2",
-                                  args=(1, 2),
-                                  kwargs={},
-                                  response=3)
+        class TestSession(ApplicationSession):
+            @inlineCallbacks
+            def onJoin(self, details):
 
-        resource = CallerResource({}, session)
+                def add2(x, y):
+                    return x + y
+
+                a = yield self.register(add2, u"com.test.add2")
+
+        session = TestSession(types.ComponentConfig(u'realm1'))
+        self.session_factory.add(session, authrole=u"test_role")
+
+        session2 = ApplicationSession(types.ComponentConfig(u'realm1'))
+        self.session_factory.add(session2, authrole=u"test_role")
+        resource = CallerResource({}, session2)
 
         with LogCapturer() as l:
             request = yield renderResource(
@@ -136,13 +190,21 @@ class CallerTestCase(TestCase):
         """
         A failed call returns the error to the client.
         """
-        session = MockSession(self)
-        session._addFailingProcedureCall("com.test.add2",
-                                         args=(1, 2),
-                                         kwargs={},
-                                         response=ApplicationError("wamp.test.broke", "broken!"))
+        class TestSession(ApplicationSession):
+            @inlineCallbacks
+            def onJoin(self, details):
 
-        resource = CallerResource({}, session)
+                def add2(x, y):
+                    raise ValueError("broken!")
+
+                a = yield self.register(add2, u"com.test.add2")
+
+        session = TestSession(types.ComponentConfig(u'realm1'))
+        self.session_factory.add(session, authrole=u"test_role")
+
+        session2 = ApplicationSession(types.ComponentConfig(u'realm1'))
+        self.session_factory.add(session2, authrole=u"test_role")
+        resource = CallerResource({}, session2)
 
         with LogCapturer() as l:
             request = yield renderResource(
@@ -154,7 +216,7 @@ class CallerTestCase(TestCase):
         self.flushLoggedErrors()
         self.assertEqual(request.code, 200)
         self.assertEqual(json.loads(native_string(request.get_written_data())),
-                         {u"error": u"wamp.test.broke", u"args": [u"broken!"],
+                         {u"error": u"wamp.error.runtime_error", u"args": [u"broken!"],
                           u"kwargs": {}})
 
         logs = l.get_category("AR458")
