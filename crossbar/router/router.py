49bacad46f14b19d242ce146a52526bea5ad36f7
Tobias Oberstein <tobias.oberstein@tavendo.de>
2016-08-08 17:44:14 +0200

    improve logging

--- crossbar/router/router.py
+++ crossbar/router/router.py
@@ -420,7 +420,7 @@ class RouterFactory(object):
                        realm=realm, role=role)
 
     def auto_start_realm(self, realm):
-        raise Exception("realm auto-activation not yet implemented")
+        raise Exception("realm auto-activation (realm '{}') not yet implemented".format(realm))
 
     def auto_add_role(self, realm, role):
-        raise Exception("role auto-activation not yet implemented")
+        raise Exception("role auto-activation (role '{}') not yet implemented".format(role))
