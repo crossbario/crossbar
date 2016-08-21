2422d5af84a0872143bf25a40febd2b773283b91
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-08-21 20:15:43 +0800

    fix pyflakes/pep8

--- crossbar/router/test/test_unisocket.py
+++ crossbar/router/test/test_unisocket.py
@@ -64,7 +64,6 @@ class UniSocketTests(TestCase):
 
         self.assertFalse(t.connected)
 
-
     def test_rawsocket_with_factory(self):
         """
         Speaking RawSocket when the connection is made will make UniSocket
@@ -93,7 +92,6 @@ class UniSocketTests(TestCase):
         self.assertTrue(t.connected)
         self.assertEqual(t.value(), b'\x7F0000000moredata')
 
-
     def test_web_with_no_factory(self):
         """
         Trying to speak HTTP without a factory will drop the connection.
@@ -109,7 +107,6 @@ class UniSocketTests(TestCase):
         self.assertTrue(t.connected)
         p.dataReceived(b'GET /foo HTTP/1.1\r\n\r\n')
         self.assertFalse(t.connected)
-
 
     def test_invalid_status_line(self):
         """
@@ -128,7 +125,6 @@ class UniSocketTests(TestCase):
         p.dataReceived(b'this is not HTTP\r\n\r\n')
         self.assertFalse(t.connected)
 
-
     def test_web_with_factory(self):
         """
         Speaking HTTP will pass it down to the HTTP factory.
@@ -137,6 +133,7 @@ class UniSocketTests(TestCase):
 
         class MyResource(Resource):
             isLeaf = True
+
             def render_GET(self, request):
                 return b"hi!"
 
@@ -154,7 +151,6 @@ class UniSocketTests(TestCase):
         self.assertFalse(t.connected)
 
         self.assertIn(b"hi!", t.value())
-
 
     def test_websocket_with_map(self):
         """
@@ -187,22 +183,13 @@ class UniSocketTests(TestCase):
         self.assertEqual(t.value(),
                          b'GET /ws HTTP/1.1\r\nConnection: close\r\n\r\n')
 
-
     def test_websocket_with_no_map(self):
         """
         A web request that matches no WebSocket
         """
         t = StringTransport()
 
-        class MyFakeWebSocket(Protocol):
-            """
-            A fake WebSocket factory which just echos data back.
-            """
-            def dataReceived(self, data):
-                self.transport.write(data)
-
-        fake_websocket = Factory.forProtocol(MyFakeWebSocket)
-        websocket_map = OrderedDict({u"x": None, u"y": None})
+        websocket_map = {u"x": None, u"y": None}
 
         f = UniSocketServerFactory(websocket_factory_map=websocket_map)
         p = f.buildProtocol(None)
