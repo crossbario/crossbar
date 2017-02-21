90bc505a3cbe05e70b981888cabae4e595af9f87
Tobias Oberstein <tobias.oberstein@tavendo.de>
2017-02-21 18:10:52 +0100

    forward authid/role

--- crossbar/router/auth/cryptosign.py
+++ crossbar/router/auth/cryptosign.py
@@ -170,6 +170,8 @@ class PendingAuthCryptosign(PendingAuth):
                 return error
 
             self._session_details[u'authmethod'] = self._authmethod  # from AUTHMETHOD, via base
+            self._session_details[u'authid'] = details.authid
+            self._session_details[u'authrole'] = details.authrole
             self._session_details[u'authextra'] = details.authextra
 
             d = self._authenticator_session.call(self._authenticator, realm, details.authid, self._session_details)
