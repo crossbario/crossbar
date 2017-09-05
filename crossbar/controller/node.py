2f2cf541a20718b63e942d0174fe0a0843c973df
Tobias Oberstein <tobias.oberstein@crossbario.com>
2017-09-05 18:29:40 +0200

    fixes #1179

--- crossbar/controller/node.py
+++ crossbar/controller/node.py
@@ -610,7 +610,8 @@ class Node(object):
 
         except Exception:
             panic = True
-            self.log.failure('Could not startup node: {log_failure.value}')
+            self.log.failure()
+            self.log.error('fatal: could not startup node')
 
         if panic:
             try:
@@ -642,6 +643,7 @@ class Node(object):
             self.log.info('Starting {nworkers} workers ...', nworkers=len(workers))
         else:
             self.log.info('No workers configured!')
+
         for worker in workers:
 
             # worker ID
@@ -651,22 +653,20 @@ class Node(object):
                 worker_id = 'worker-{:03d}'.format(self._worker_no)
                 self._worker_no += 1
 
-            # worker type - a type of working process from the following fixed list
+            # worker type: either a native worker ('router', 'container', ..), or a guest worker ('guest')
             worker_type = worker['type']
-            assert worker_type in self._native_workers
-            worker_cfg = self._native_workers[worker_type]
-
-            # set logname depending on worker type
-            worker_logname = "{} '{}'".format(worker_cfg['logname'], worker_id)
-
-            # any worker specific options
-            worker_options = worker.get('options', {})
-
-            # now actually start the worker ..
-            yield self._controller.call(u'crossbar.start_worker', worker_id, worker_type, worker_options, options=CallOptions())
 
             # native worker processes setup
             if worker_type in self._native_workers:
+
+                # set logname depending on native worker type
+                worker_logname = '{} "{}"'.format(self._native_workers[worker_type]['logname'], worker_id)
+
+                # any worker specific options
+                worker_options = worker.get('options', {})
+
+                # now actually start the (native) worker ..
+                yield self._controller.call(u'crossbar.start_worker', worker_id, worker_type, worker_options, options=CallOptions())
 
                 # setup native worker generic stuff
                 method_name = '_configure_native_worker_{}'.format(worker_type.replace('-', '_'))
@@ -678,6 +678,18 @@ class Node(object):
                         "there is no method '{}' on {}".format(worker_type, method_name, type(self))
                     )
                 yield config_fn(worker_logname, worker_id, worker)
+
+            # guest worker processes setup
+            elif worker_type == u'guest':
+
+                # now actually start the (guest) worker ..
+
+                # FIXME: start_worker() takes the whole configuration item for guest workers, whereas native workers
+                # only take the options (which is part of the whole config item for the worker)
+                yield self._controller.call(u'crossbar.start_worker', worker_id, worker_type, worker, options=CallOptions())
+
+            else:
+                raise Exception('logic error: unexpected worker_type="{}"'.format(worker_type))
 
         self.log.info('Local node configuration applied successfully!')
 
