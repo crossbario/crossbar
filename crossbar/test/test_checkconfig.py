crossbario/crossbar

commit ff61c0ad7da1dcfa36859aee6d851395fcecfac3

rectify some things

crossbar/common/checkconfig.py
@@ -881,14 +881,14 @@ def check_listening_endpoint_onion(endpoint):
     check_dict_args(
         {
             u"type": (True, [six.text_type]),
-            u"port": (True, [int, long]),
+            u"port": (True, six.integer_types),
             u"private_key_file": (True, [six.text_type]),
             u"tor_control_endpoint": (True, [Mapping])
         },
         endpoint,
         "onion endpoint config",
     )
-
+    check_endpoint_port(u"port")
     check_connecting_endpoint(endpoint[u"tor_control_endpoint"])
 
 

crossbar/__init__.py
@@ -33,6 +33,6 @@ from __future__ import absolute_import
 import txaio
 txaio.use_twisted()
 
-__version__ = '17.8.1.post1'
+__version__ = '17.8.2.dev1'
 
 __all__ = ('__version__',)

crossbar/worker/process.py
@@ -32,9 +32,34 @@ from __future__ import absolute_import, print_function
 
 import six
 
+import pkg_resources
+
 from twisted.internet.error import ReactorNotRunning
 
 __all__ = ('run',)
+
+
+def get_worker_classes():
+    """
+    Get Worker classes which implement worker types.
+
+    :returns: Dict with worker classes and aux info.
+    :rtype: dict
+    """
+    res = {}
+    for entrypoint in pkg_resources.iter_entry_points('crossbar.worker'):
+        e = entrypoint.load()
+        ep = {
+            u'class': e,
+            u'dist': entrypoint.dist.key,
+            u'version': entrypoint.dist.version,
+        }
+        if hasattr(e, '__doc__') and e.__doc__:
+            ep[u'doc'] = e.__doc__.strip()
+        else:
+            ep[u'doc'] = None
+        res[entrypoint.name] = ep
+    return res
 
 
 def run():
@@ -175,11 +200,21 @@ def run():
     from crossbar.worker.container import ContainerWorkerSession
     from crossbar.worker.testee import WebSocketTesteeWorkerSession
 
+    # default worker type classes
     WORKER_TYPE_TO_CLASS = {
-        'router': RouterWorkerSession,
-        'container': ContainerWorkerSession,
-        'websocket-testee': WebSocketTesteeWorkerSession
+        u'router': RouterWorkerSession,
+        u'container': ContainerWorkerSession,
+        u'websocket-testee': WebSocketTesteeWorkerSession
     }
+
+    # get new worker type classes, or worker type class overrides from the
+    # plugin system
+    worker_classes = get_worker_classes()
+    for ep, worker_class in worker_classes.items():
+        klass = worker_class['class']
+        worker_type = klass.WORKER_TYPE
+        WORKER_TYPE_TO_CLASS[worker_type] = klass
+        log.info('Worker type {worker_type} configured with class {worker_class}', worker_type=worker_type, worker_class=klass)
 
     from twisted.internet.error import ConnectionDone
     from autobahn.twisted.websocket import WampWebSocketServerProtocol

crossbar/worker/router.py
@@ -286,6 +286,7 @@ class RouterWorkerSession(NativeWorkerSession):
     multiple (embedded) application components.
     """
     WORKER_TYPE = u'router'
+    PROCS = []
 
     def __init__(self, config=None, reactor=None):
         NativeWorkerSession.__init__(self, config, reactor)
@@ -323,7 +324,7 @@ class RouterWorkerSession(NativeWorkerSession):
         yield NativeWorkerSession.onJoin(self, details, publish_ready=False)
 
         # the procedures registered
-        procs = [
+        self.PROCS.extend([
             u'get_router_realms',
             u'start_router_realm',
             u'stop_router_realm',
@@ -343,12 +344,12 @@ class RouterWorkerSession(NativeWorkerSession):
             u'get_router_transports',
             u'start_router_transport',
             u'stop_router_transport',
-        ]
+        ])
 
         dl = []
-        for proc in procs:
+        for proc in self.PROCS:
             uri = u'{}.{}'.format(self._uri_prefix, proc)
-            self.log.debug('Registering management API procedure <{proc}>', proc=uri)
+            self.log.info('Registering management API procedure <{proc}>', proc=uri)
             dl.append(self.register(getattr(self, proc), uri, options=RegisterOptions(details_arg='details')))
 
         regs = yield DeferredList(dl)

commit e576ec1bd683a62d85945b11e43e7dc770765848

Fix checking of onion-endpoint public ports + tests
Broken by ff61c0ad

crossbar/common/checkconfig.py
@@ -888,7 +888,7 @@ def check_listening_endpoint_onion(endpoint):
         endpoint,
         "onion endpoint config",
     )
-    check_endpoint_port(u"port")
+    check_endpoint_port(endpoint[u"port"])
     check_connecting_endpoint(endpoint[u"tor_control_endpoint"])
 
 

crossbar/test/test_checkconfig.py
@@ -414,3 +414,43 @@ class CheckRealmTests(TestCase):
             checkconfig.InvalidConfigException,
             checkconfig.check_router_realm, config_realm,
         )
+
+
+class CheckOnion(TestCase):
+
+    def test_unknown_attr(self):
+        with self.assertRaises(checkconfig.InvalidConfigException) as ctx:
+            checkconfig.check_listening_endpoint_onion({
+                u"type": u"onion",
+                u"foo": 42,
+            })
+        self.assertIn(
+            "unknown attribute",
+            str(ctx.exception)
+        )
+
+    def test_success(self):
+        checkconfig.check_listening_endpoint_onion({
+            u"type": u"onion",
+            u"private_key_file": u"something",
+            u"port": 1234,
+            u"tor_control_endpoint": {
+                u"type": u"unix",
+                u"path": u"/dev/null",
+            }
+        })
+
+    def test_port_wrong_type(self):
+        with self.assertRaises(checkconfig.InvalidConfigException) as ctx:
+            checkconfig.check_listening_endpoint_onion({
+                u"type": u"onion",
+                u"port": u"1234",
+            })
+        self.assertIn(
+            "invalid type",
+            str(ctx.exception)
+        )
+        self.assertIn(
+            "encountered for attribute 'port'",
+            str(ctx.exception)
+        )