e6ca880d52cb69971acffe755469446d6ede575f
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-04-29 13:07:11 +0200

    one more try

--- test/test_ab_examples.sh
+++ test/test_ab_examples.sh
@@ -4,8 +4,7 @@ cd /tmp
 rm -f master.zip
 curl -o master.zip https://codeload.github.com/crossbario/autobahn-python/zip/master
 rm -rf autobahn-python-master
-unzip master.zip
-ls -la autobahn-python-master/examples
+unzip -q master.zip
 cd autobahn-python-master/examples
 
 # run RawSocket tests using Twisted and asyncio Autobahn client
