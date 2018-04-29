132661de21bd159e7fac6480f3b520a896f046c2
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-04-29 11:31:46 +0200

    sync

--- test/test_ab_examples.sh
+++ test/test_ab_examples.sh
@@ -7,5 +7,9 @@ rm -rf autobahn-python-master
 unzip master.zip
 ls -la autobahn-python-master/examples
 cd autobahn-python-master/examples
+
+# run RawSocket tests using Twisted and asyncio Autobahn client
 AUTOBAHN_DEMO_ROUTER=rs://127.0.0.1:8080 python run-all-examples.py
+
+# run WebSocket tests using Twisted and asyncio Autobahn client
 AUTOBAHN_DEMO_ROUTER=ws://127.0.0.1:8080/ws python run-all-examples.py
