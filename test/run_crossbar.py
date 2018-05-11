59ba6b415636c7c118fa8d976dfc187c654399d3
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-05-11 15:34:01 +0200

    fix: dir renaming in manifest

--- test/run_crossbar.py
+++ test/run_crossbar.py
@@ -1,7 +1,3 @@
 if __name__ == '__main__':
-    from pkg_resources import load_entry_point
-    import sys
-
-    sys.exit(
-        load_entry_point('crossbar', 'console_scripts', 'crossbar')()
-    )
+    from crossbar import run
+    run()
