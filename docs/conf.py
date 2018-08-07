5391405d367a15c3079079c367e81002249427e9
codelectron <codelectron@gmail.com>
2018-08-07 22:31:57 +0200

    remove sphinx-tabs, not used now

--- docs/conf.py
+++ docs/conf.py
@@ -148,4 +148,3 @@ html_static_path = ['_static']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
-extensions = ['sphinx_tabs.tabs']
