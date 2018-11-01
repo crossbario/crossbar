f060e4557378e3b01fa44a80aca2d89fe2b4a077
codelectron <codelectron@gmail.com>
2018-11-01 22:39:46 +0100

    add updated getting-started, cosmetic change to svg and remove receive count to frontend

--- docs/pages/code/frontend.js
+++ docs/pages/code/frontend.js
@@ -12,18 +12,9 @@ var connection = new autobahn.Connection({
 
 connection.onopen = function (session) {
 
-   var received = 0;
-   var textmessage =  "Closed after recieving 5 events, Hit F5 to refresh";
-
    function onevent1(args) {
       console.log("Got event:", args[0]);
       document.getElementById('WAMPEvent').innerHTML = "Events:"+ args[0];
-      received += 1;
-      if (received > 5) {
-         console.log("Closing ..");
-         document.getElementById('WAMPEvent').textContent= textmessage;
-         connection.close();
-      }
    }
 
    session.subscribe('com.myapp.hello', onevent1);
