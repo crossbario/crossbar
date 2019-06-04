1359c5bd430fb0a15a5364cacf7a5a86dd63a34d
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-06-04 17:38:17 +0200

    guard attribute access (#1594)

--- crossbar/router/service.py
+++ crossbar/router/service.py
@@ -252,8 +252,8 @@ class RouterServiceAgent(ApplicationSession):
         if session_id in self._router._session_id_to_session:
             session = self._router._session_id_to_session[session_id]
             if not is_restricted_session(session):
-                session_info = session._session_details.marshal() if session._session_details else dict()
-                session_info[u'transport'] = None if session._transport is None else session._transport._transport_info
+                session_info = session._session_details.marshal() if hasattr(session, '_session_details') else dict()
+                session_info[u'transport'] = session._transport._transport_info if hasattr(session, '_transport') and hasattr(session._transport, '_transport_info') else None
                 return session_info
             else:
                 self.log.warn('wamp.session.get: denied returning restricted session {session_id}', session_id=session_id)
