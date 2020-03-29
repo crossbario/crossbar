2fa727658b0486167172be05de7dde2d4ec0df29
meejah <meejah@meejah.ca>
2020-03-28 20:06:09 -0600

    add anonymous-proxy and cryptosign-proxy authmethods

--- crossbar/common/checkconfig.py
+++ crossbar/common/checkconfig.py
@@ -609,6 +609,7 @@ def check_transport_auth(personality, auth, ignore=[], checks=None):
         'tls': check_transport_auth_tls,
         'cookie': check_transport_auth_cookie,
         'cryptosign': check_transport_auth_cryptosign,
+        'cryptosign-proxy': check_transport_auth_cryptosign,
         'scram': check_transport_auth_scram,
 
         # FIXME: these are actually not the same as corresponding non-proxied configuration items
