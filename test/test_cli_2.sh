078ea7ad452e3316f451e9a7a80632852fd97bfe
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-04-07 11:12:45 +0200

    fix

--- test/test_cli_2.sh
+++ test/test_cli_2.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # CROSSBAR_FABRIC_URL=ws://localhost:9000/ws
-# CROSSBAR_FABRIC_SUPERUSER=${HOME}/.crossbarfx/default.pub
+# CROSSBAR_FABRIC_SUPERUSER=${HOME}/.crossbar/default.pub
 
 echo "Using CROSSBAR_FABRIC_URL=${CROSSBAR_FABRIC_URL}"
 echo "Using CROSSBAR_FABRIC_SUPERUSER=${CROSSBAR_FABRIC_SUPERUSER}"
 
-# this will create ~/.crossbarfx/* if it doesn't yet exist
+# this will create ~/.crossbar/* if it doesn't yet exist
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Authenticate the CLI client (shell) and print version info: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell init --yes
-crossbarfx master version
+crossbar shell init --yes
+crossbar master version
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Stop all nodes (if any are running) and scratch all data: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
@@ -18,89 +18,89 @@ mkdir -p ./test/cfc/.crossbar
 mkdir -p ./test/cf1/.crossbar
 mkdir -p ./test/cf2/.crossbar
 mkdir -p ./test/cf3/.crossbar
-crossbarfx edge stop --cbdir ./test/cf1/.crossbar
-crossbarfx edge stop --cbdir ./test/cf2/.crossbar
-crossbarfx edge stop --cbdir ./test/cf3/.crossbar
-crossbarfx master stop --cbdir ./test/cfc/.crossbar
+crossbar edge stop --cbdir ./test/cf1/.crossbar
+crossbar edge stop --cbdir ./test/cf2/.crossbar
+crossbar edge stop --cbdir ./test/cf3/.crossbar
+crossbar master stop --cbdir ./test/cfc/.crossbar
 rm -rf ./test/cfc/.crossbar/.db-*
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Start the master node: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx master start --cbdir ./test/cfc/.crossbar &
+crossbar master start --cbdir ./test/cfc/.crossbar &
 sleep 5
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Authenticate the CLI client (shell): >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell init --yes
+crossbar shell init --yes
 echo "\n>>>>>>> Create a new mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell create mrealm mrealm1
+crossbar shell create mrealm mrealm1
 echo "\n>>>>>>> List mrealms, this must have 1 mrealm entry: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell list mrealms
+crossbar shell list mrealms
 sleep 3
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Pairing nodes with mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
-crossbarfx shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
-crossbarfx shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
+crossbar shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
+crossbar shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
+crossbar shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
 echo "\n>>>>>>> Nodes paired! The list of nodes in the mrealm must have all nodes: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 list nodes
+crossbar shell --realm mrealm1 list nodes
 echo "\n>>>>>>> Check each node: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 show node node1
-crossbarfx shell --realm mrealm1 show node node2
-crossbarfx shell --realm mrealm1 show node node3
+crossbar shell --realm mrealm1 show node node1
+crossbar shell --realm mrealm1 show node node2
+crossbar shell --realm mrealm1 show node node3
 sleep 2
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Unpairing all nodes from mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell unpair node ./test/cf1/.crossbar/key.pub
-crossbarfx shell unpair node ./test/cf2/.crossbar/key.pub
-crossbarfx shell unpair node ./test/cf3/.crossbar/key.pub
+crossbar shell unpair node ./test/cf1/.crossbar/key.pub
+crossbar shell unpair node ./test/cf2/.crossbar/key.pub
+crossbar shell unpair node ./test/cf3/.crossbar/key.pub
 echo "\n>>>>>>> Nodes unpaired! The list of nodes in the mrealm must be empty: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 list nodes
+crossbar shell --realm mrealm1 list nodes
 echo "\n>>>>>>> Deleting the mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell delete mrealm mrealm1
+crossbar shell delete mrealm mrealm1
 sleep 2
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Re-create the mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell create mrealm mrealm1
+crossbar shell create mrealm mrealm1
 echo "\n>>>>>>> Re-Pairing nodes with mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
-crossbarfx shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
-crossbarfx shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
+crossbar shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
+crossbar shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
+crossbar shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
 echo "\n>>>>>>> Nodes re-paired! The list of nodes in the mrealm must have all nodes: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 list nodes
+crossbar shell --realm mrealm1 list nodes
 echo "\n>>>>>>> Check each node: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 show node node1
-crossbarfx shell --realm mrealm1 show node node2
-crossbarfx shell --realm mrealm1 show node node3
+crossbar shell --realm mrealm1 show node node1
+crossbar shell --realm mrealm1 show node node2
+crossbar shell --realm mrealm1 show node node3
 sleep 2
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Deleting the mrealm (with CASCADE unpair of all nodes): >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell delete mrealm mrealm1 --cascade
+crossbar shell delete mrealm mrealm1 --cascade
 echo "\n>>>>>>> Mrealm deleted! The list of mrealms must be empty: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell list mrealms
+crossbar shell list mrealms
 sleep 2
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Re-create the mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell create mrealm mrealm1
+crossbar shell create mrealm mrealm1
 echo "\n>>>>>>> Re-pairing nodes with mrealm: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
-crossbarfx shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
-crossbarfx shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
+crossbar shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
+crossbar shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
+crossbar shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
 echo "\n>>>>>>> Nodes paired! The list of nodes in the mrealm must have all nodes: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 list nodes
+crossbar shell --realm mrealm1 list nodes
 echo "\n>>>>>>> Check each node: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell --realm mrealm1 show node node1
-crossbarfx shell --realm mrealm1 show node node2
-crossbarfx shell --realm mrealm1 show node node3
+crossbar shell --realm mrealm1 show node node1
+crossbar shell --realm mrealm1 show node node2
+crossbar shell --realm mrealm1 show node node3
 sleep 2
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Stop the master node: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx master stop --cbdir ./test/cfc/.crossbar
+crossbar master stop --cbdir ./test/cfc/.crossbar
 
 echo "\n################################################################################################################################################################"
--- test/test_setup.sh
+++ test/test_setup.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 
 # CROSSBAR_FABRIC_URL=ws://localhost:9000/ws
-# CROSSBAR_FABRIC_SUPERUSER=${HOME}/.crossbarfx/default.pub
+# CROSSBAR_FABRIC_SUPERUSER=${HOME}/.crossbar/default.pub
 
 echo "Using CROSSBAR_FABRIC_URL=${CROSSBAR_FABRIC_URL}"
 echo "Using CROSSBAR_FABRIC_SUPERUSER=${CROSSBAR_FABRIC_SUPERUSER}"
 
-# this will create ~/.crossbarfx/* if it doesn't yet exist
-crossbarfx shell init --yes
-crossbarfx master version
+# this will create ~/.crossbar/* if it doesn't yet exist
+crossbar shell init --yes
+crossbar master version
 
 # start CFC node
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Start the master node >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-CROSSBARFX_METERING_URL="" CROSSBARFX_DISABLE_CE=1 crossbarfx master start --cbdir ./test/cfc/.crossbar &
+crossbar_METERING_URL="" crossbar_DISABLE_CE=1 crossbar master start --cbdir ./test/cfc/.crossbar &
 sleep 5
 echo "\n>>>>>>> Master node started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 # authenticate, create new management realm and pair the 3 CF nodes
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Creating mrealm >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell init --yes
-crossbarfx shell create mrealm mrealm1
+crossbar shell init --yes
+crossbar shell create mrealm mrealm1
 sleep 5
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Pairing nodes >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx shell list mrealms
-crossbarfx shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
-crossbarfx shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
-crossbarfx shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
+crossbar shell list mrealms
+crossbar shell pair node ./test/cf1/.crossbar/key.pub mrealm1 node1
+crossbar shell pair node ./test/cf2/.crossbar/key.pub mrealm1 node2
+crossbar shell pair node ./test/cf3/.crossbar/key.pub mrealm1 node3
 
 echo "\n################################################################################################################################################################"
 echo "\n>>>>>>> Stop master node >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-crossbarfx master stop --cbdir ./test/cfc/.crossbar
+crossbar master stop --cbdir ./test/cfc/.crossbar
