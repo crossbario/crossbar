d0ca98eb315c018e55bce9f3d8c97303baa59eae
Tobias Oberstein <tobias.oberstein@crossbario.com>
2021-05-30 18:05:11 +0200

    expand WAP web service

--- crossbar/webservice/wap.py
+++ crossbar/webservice/wap.py
@@ -7,7 +7,9 @@
 
 import os
 import importlib
+import json
 import pkg_resources
+from pprint import pformat
 
 from collections.abc import Mapping, Sequence
 
@@ -24,6 +26,7 @@ from twisted.web import resource
 from twisted.web import server
 from twisted.internet.defer import succeed
 
+from autobahn.wamp.serializer import JsonObjectSerializer
 from autobahn.wamp.types import ComponentConfig
 from autobahn.wamp.exception import ApplicationError
 from autobahn.twisted.wamp import ApplicationSession
@@ -45,6 +48,8 @@ class WapResource(resource.Resource):
     """
 
     log = make_logger()
+
+    ser = JsonObjectSerializer()
 
     isLeaf = True
 
@@ -152,7 +157,11 @@ class WapResource(resource.Resource):
             if rpath != '/':
                 _rp.append(rpath)
             route_url = '/' + '/'.join(_rp)
-            route_methods = [route.get('method')]
+
+            route_method = route.get('method', 'GET')
+            assert route_method in ['GET', 'POST'], 'invalid HTTP method "{}" for route on URL "{}"'.format(route_method, route_url)
+
+            route_methods = [route_method]
 
             # note the WAMP procedure to call and the Jinja2 template to render as HTTP response
             route_endpoint = (route['call'], env.get_template(route['render']))
@@ -167,41 +176,46 @@ class WapResource(resource.Resource):
         # http://werkzeug.pocoo.org/docs/dev/routing/#werkzeug.routing.MapAdapter.match
         self._map_adapter = map.bind('/')
 
-    def _after_call_success(self, result, request):
+    def _after_call_success(self, result, request, client_return_json):
         """
         When the WAMP call attached to the URL returns, render the WAMP result
-        into a Jinja2 template and return HTML to client.
+        into a Jinja2 template and return HTML to client. Alternatively, return
+        the call result as plain JSON.
 
-        :param payload: The dict returned from the topic
+        :param result: The dict returned from the WAMP procedure call.
         :param request: The HTTP request.
-        :return: server.NOT_DONE_YET (special)
+        :param client_return_json: Flag indicating to return plain JSON (no HTML rendering.)
         """
         try:
-            rendered_html = request.template.render(result)
+            if client_return_json:
+                rendered = self.ser.serialize(result)
+                self.log.info('WapResource successfully serialized JSON result:\n{result}', result=pformat(result))
+            else:
+                rendered = request.template.render(result).encode('utf8')
+                self.log.info('WapResource successfully rendered HTML result:\n{rendered}', rendered=rendered)
         except Exception as e:
             self.log.failure()
-            emsg = 'WabResource render error for WAMP result of type "{}": {}'.format(type(result), e)
+            emsg = 'WapResource render error for WAMP result of type "{}": {}'.format(type(result), e)
             self.log.warn(emsg)
             request.setResponseCode(500)
-            request.write(self._render_error(emsg, request))
+            request.write(self._render_error(emsg, request, client_return_json))
         else:
-            request.write(rendered_html.encode('utf8'))
+            request.write(rendered)
         request.finish()
 
-    def _after_call_error(self, error, request):
+    def _after_call_error(self, error, request, client_return_json):
         """
         Deferred error, write out the error template and finish the request
 
         :param error: The current deferred error object
         :param request: The original HTTP request
-        :return: None
         """
         self.log.error('WapResource error: {error}', error=error)
         request.setResponseCode(500)
-        request.write(self._render_error(error.value.error, request))
+        request.write(self._render_error(error.value.error, request, client_return_json))
         request.finish()
 
-    def _render_error(self, message, request):
+    def _render_error(self, message, request, client_return_json=False):
         """
         Error renderer, display a basic error message to tell the user that there
         was a problem and roughly what the problem was.
@@ -210,17 +224,20 @@ class WapResource(resource.Resource):
         :param request: The original HTTP request
         :return: HTML formatted error string
         """
-        return """
-            <html>
-                <title>API Error</title>
-                <body>
-                    <h3 style="color: #f00">Crossbar WAMP Application Page Error</h3>
-                    <pre>{}</pre>
-                </body>
-            </html>
-        """.format(escape(message)).encode('utf8')
+        if client_return_json:
+            return self.ser.serialize({'error': message})
+        else:
+            return """
+                <html>
+                    <title>API Error</title>
+                    <body>
+                        <h3 style="color: #f00">Crossbar WAMP Application Page Error</h3>
+                        <pre>{}</pre>
+                    </body>
+                </html>
+            """.format(escape(message)).encode('utf8')
 
-    def render_GET(self, request):
+    def render(self, request):
         """
         Initiate the rendering of a HTTP/GET request by calling a WAMP procedure, the
         resulting ``dict`` is rendered together with the specified Jinja2 template
@@ -229,6 +246,30 @@ class WapResource(resource.Resource):
         :param request: The HTTP request.
         :returns: server.NOT_DONE_YET (special)
         """
+        # https://twistedmatrix.com/documents/current/api/twisted.web.resource.Resource.html#render
+        full_path = request.uri.decode('utf-8')
+        http_method = request.method.decode()
+
+        if http_method not in ['GET', 'POST']:
+            request.setResponseCode(511)
+            return self._render_error(
+                'Method not allowed on path "{full_path}" [werkzeug.routing.MapAdapter.match]'.format(
+                    full_path=full_path), request)
+
+        if http_method == 'POST' and request.content:
+            # https://stackoverflow.com/a/11549600/884770
+            # http://marianoiglesias.com.ar/python/file-uploading-with-multi-part-encoding-using-twisted/
+            body_data = request.content.read()
+            self.log.info('POST data len = {newdata_len}', newdata_len=len(body_data))
+        else:
+            body_data = None
+
+        client_accept = request.getAllHeaders().get(b'accept', None)
+        if client_accept:
+            client_accept = client_accept.decode()
+
+        client_return_json = client_accept == 'application/json'
+
         cookie = request.received_cookies.get(b'session_cookie')
         self.log.debug('Session Cookie is ({})'.format(cookie))
         if cookie:
@@ -250,31 +291,36 @@ class WapResource(resource.Resource):
             self.log.error('could not call procedure - no session')
             return self._render_error('could not call procedure - no session', request)
 
-        full_path = request.uri.decode('utf-8')
         try:
             # werkzeug.routing.MapAdapter
             # http://werkzeug.pocoo.org/docs/dev/routing/#werkzeug.routing.MapAdapter.match
-            (procedure, request.template), kwargs = self._map_adapter.match(full_path)
+            (procedure, request.template), kwargs = self._map_adapter.match(full_path, method=http_method)
 
-            self.log.debug('WapResource HTTP/GET "{full_path}" mapped to procedure "{procedure}"',
-                           full_path=full_path,
-                           procedure=procedure)
+            self.log.info('WapResource on path "{full_path}" mapped to procedure "{procedure}"',
+                          full_path=full_path,
+                          procedure=procedure)
 
             if procedure:
+                self.log.info('calling procedure "{procedure}" with {kwargs}, {body_data_len}',
+                              procedure=procedure, kwargs=kwargs, body_data_len=len(body_data) if body_data else 0)
                 # FIXME: how do we allow calling WAMP procedures with positional args?
-                if kwargs:
-                    d = session.call(procedure, **kwargs)
+                if body_data:
+                    if kwargs:
+                        d = session.call(procedure, **kwargs, body_data=body_data)
+                    else:
+                        d = session.call(procedure, body_data=body_data)
                 else:
-                    d = session.call(procedure)
+                    if kwargs:
+                        d = session.call(procedure, **kwargs)
+                    else:
+                        d = session.call(procedure)
             else:
                 d = succeed({})
 
-            # d.addCallback(self._after_call_success, request)
-            # d.addErrback(self._after_call_error, request)
             d.addCallbacks(self._after_call_success,
                            self._after_call_error,
-                           callbackArgs=[request],
-                           errbackArgs=[request])
+                           callbackArgs=[request, client_return_json],
+                           errbackArgs=[request, client_return_json])
 
             return server.NOT_DONE_YET
 
