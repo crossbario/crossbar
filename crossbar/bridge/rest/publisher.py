bc59e8567e293c3eb2611b3a223a2a558f113e4b
yankos <yankos.kos@gmail.com>
2022-06-23 10:43:29 +0300

    MODIFY more pretty diff. ref #1829

--- crossbar/bridge/rest/publisher.py
+++ crossbar/bridge/rest/publisher.py
@@ -29,15 +29,15 @@ class PublisherResource(_CommonResource):
         options = event['options'] if 'options' in event and event['options'] else {}
 
         publish_options = PublishOptions(acknowledge=True,
+                                         forward_for=options.get('forward_for', None),
+                                         retain=options.get('retain', None),
                                          exclude_me=options.get('exclude_me', None),
-                                         exclude=options.get('exclude', None),
                                          exclude_authid=options.get('exclude_authid', None),
                                          exclude_authrole=options.get('exclude_authrole', None),
-                                         eligible=options.get('eligible', None),
+                                         exclude=options.get('exclude', None),
                                          eligible_authid=options.get('eligible_authid', None),
                                          eligible_authrole=options.get('eligible_authrole', None),
-                                         retain=options.get('retain', None),
-                                         forward_for=options.get('forward_for', None))
+                                         eligible=options.get('eligible', None))
 
         kwargs['options'] = publish_options
 
