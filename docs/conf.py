d157f48b09550a68e0e30be48570eb1dfe563b3c
David Ellis <david.ellis@atamate.com>
2024-12-11 16:28:13 +0000

    intersphinx_mapping format has changed

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ html_favicon = '_static/favicon.ico'
 # -- Options for intersphinx extension ---------------------------------------
 
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
+    'python': ('https://docs.python.org/', None),
     'py2': ('https://docs.python.org/2', None),
     'py3': ('https://docs.python.org/3', None),
 
