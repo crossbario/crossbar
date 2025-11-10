ed48ddbbb503c7b4315c8766883d9565c5ffb005
Tobias Oberstein <tobias.oberstein@gmail.com>
2025-11-10 22:18:50 +0100

    Complete migration from requirements.txt to pyproject.toml
    Remove all requirements*.txt files and complete the migration to modern
    Python packaging with pyproject.toml as the single source of truth.
    
    Changes:
    
    1. pyproject.toml:
       - Add furo>=2024.1.29 to dev dependencies for Sphinx theme
    
    2. setup.py:
       - Simplify to minimal shim that defers to pyproject.toml
       - Remove code that read from requirements*.txt files
       - Keep only environment variable setup (LMDB_FORCE_CFFI, etc.)
       - Modern setuptools reads all metadata from pyproject.toml
    
    3. GitHub Actions workflows:
       - deploy-docs.yml: Replace 'pip install -r requirements-dev.txt' with 'pip install -e .[dev]'
       - deploy-wheels.yml: Replace 'pip install -r requirements-dev.txt' with 'pip install -e .[dev]'
       - test-management.yml: Replace requirements files with 'pip install -e .[dev,dev-latest]'
    
    4. Remove files:
       - requirements-dev.txt (now in [project.optional-dependencies] dev)
       - requirements-latest.txt (now in [project.optional-dependencies] dev-latest)
       - requirements-min.txt (no longer used)
       - requirements-pinned.txt (no longer used)
       - requirements.txt (dependencies now in [project] dependencies)
    
    Installation:
    - Standard: pip install .
    - Development: pip install -e .[dev]
    - Latest from GitHub: pip install -e .[dev,dev-latest]
    - Local editable: just install-dev-local cpy311
    
    Benefits:
    - Single source of truth for all package metadata
    - Modern PEP 517/518/621 compliant packaging
    - Simpler maintenance (no duplicate dependency lists)
    - Better tool support (modern pip, build, twine)
    
    Supersedes commit 0f598dd9

--- setup.py
+++ setup.py
@@ -7,53 +7,19 @@
 
 # IMPORTANT:
 #
-# 1. You must use pip 22.0.3 or later
-#       pls https://github.com/crossbario/crossbar/pull/1943#issuecomment-1037569885
+# This setup.py is kept for backwards compatibility with older pip versions.
+# All package metadata and dependencies are now defined in pyproject.toml.
 #
-# 2. No, we can't add that pip version requirement here
-#       see https://stackoverflow.com/a/60159094/884770
+# Modern installation (pip >= 22.0.3):
+#   pip install .
+#   pip install -e .[dev]
+#   pip install -e .[dev,dev-latest]
 #
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-# read package description
-with open('README.rst') as f:
-    long_description = f.read()
-
-# read package version
-with open('crossbar/_version.py') as f:
-    exec(f.read())  # defines __version__
-
-install_requires = []
-
-extras_require = {
-    'dev': []
-}
-with open('requirements-dev.txt') as f:
-    for line in f.read().splitlines():
-        extras_require['dev'].append(line.strip())
-
-# with open('requirements-min.txt') as f:
-with open('requirements-latest.txt') as f:
-    for line in f.read().splitlines():
-        line = line.strip()
-        if not line.startswith('#'):
-            parts = line.strip().split(';')
-            if len(parts) > 1:
-                parts[0] = parts[0].strip()
-                parts[1] = ':{}'.format(parts[1].strip())
-                if parts[1] not in extras_require:
-                    extras_require[parts[1]] = []
-                extras_require[parts[1]].append(parts[0])
-            else:
-                name = parts[0]
-                # do NOT (!) touch this!
-                # {package-name}[flavor] @ git+https://github.com/{user|org}/{repository}.git@{tag}#egg={package-name}
-                # https://peps.python.org/pep-0508/
-                install_requires.append(name)
 
 # enforce use of CFFI for LMDB
 os.environ['LMDB_FORCE_CFFI'] = '1'
@@ -64,63 +30,6 @@ os.environ['SODIUM_INSTALL'] = 'bundled'
 # enforce use of pure Python py-ubjson (no Cython)
 os.environ['PYUBJSON_NO_EXTENSION'] = '1'
 
-# now actually call into setuptools ..
-setup(
-    name='crossbar',
-    version=__version__,
-    description='Crossbar.io multi-protocol (WAMP/WebSocket, REST/HTTP, MQTT) application router for microservices.',
-    long_description=long_description,
-    author='typedef int GmbH',
-    url='http://crossbar.io/',
-    platforms=('Any'),
-    license="European Union Public Licence 1.2 (EUPL 1.2)",
-
-    # unfortunately, dependencies refered directly or indirectly (!)
-    # from packages listed here will take precedence over packages
-    # refered to via "git+https" based package repository links
-    # regardless of which package version is newer!
-    # https://stackoverflow.com/questions/17366784/setuptools-unable-to-use-link-from-dependency-links
-    install_requires=install_requires,
-
-    extras_require=extras_require,
-    entry_points={
-        # CLI entry function
-        'console_scripts': [
-            'crossbar = crossbar:run'
-        ]
-    },
-    packages=find_packages(),
-    include_package_data=True,
-    data_files=[('.', ['crossbar/LICENSE', 'crossbar/LICENSES-OSS', 'crossbar.ico', 'requirements-dev.txt',
-                       'requirements-latest.txt', 'requirements-min.txt', 'requirements-pinned.txt', 'requirements.txt'])],
-    zip_safe=False,
-    python_requires='>=3.7',
-
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=["License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
-                 "Development Status :: 5 - Production/Stable",
-                 "Environment :: No Input/Output (Daemon)",
-                 "Environment :: Console",
-                 "Framework :: Twisted",
-                 "Intended Audience :: Developers",
-                 "Operating System :: OS Independent",
-                 "Programming Language :: Python :: 3.7",
-                 "Programming Language :: Python :: 3.8",
-                 "Programming Language :: Python :: 3.9",
-                 "Programming Language :: Python :: 3.10",
-                 "Programming Language :: Python :: 3.11",
-                 "Programming Language :: Python :: Implementation :: CPython",
-                 "Programming Language :: Python :: Implementation :: PyPy",
-                 "Topic :: Internet",
-                 "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
-                 "Topic :: Communications",
-                 "Topic :: Database",
-                 "Topic :: Home Automation",
-                 "Topic :: Software Development :: Libraries",
-                 "Topic :: Software Development :: Libraries :: Application Frameworks",
-                 "Topic :: Software Development :: Embedded Systems",
-                 "Topic :: Software Development :: Object Brokering",
-                 "Topic :: System :: Distributed Computing",
-                 "Topic :: System :: Networking"],
-    keywords='crossbar router autobahn autobahn.ws websocket realtime rfc6455 wamp rpc pubsub oracle postgres postgresql'
-)
+# All configuration is now in pyproject.toml
+# setuptools will automatically read from pyproject.toml when using build backend
+setup()
